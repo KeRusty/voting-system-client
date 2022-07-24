85decaec754f534bfeab94d2698f06c60d0f9c97
Chathu Abeywickrama <chathu.abey93@gmail.com>
2022-07-24 14:56:37 +0400

    Landing Pages created

---
+++ src/Views/Landing/Landing.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// Styles
+import "./Landing.scss";
+
+function Landing() {
+  return (
+    <div>
+      <h1>TEST</h1>
+    </div>
+  );
+}
+
+export default Landing;
